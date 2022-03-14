c43378f78d06e18aa90105a390f9fa7099406565
Emiliano Corsaro <emii.corsaro@gmail.com>
2022-03-14 19:01:27 -0300

    Adding swagger tool

--- InvitationPageAPI/Program.cs
+++ InvitationPageAPI/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,5 +15,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwagger();
+app.MapSwagger();
+app.UseSwaggerUI();
 
 app.Run();
