7159f82d28ddd99961318d903d27838b602b545b
Emiliano Corsaro <emii.corsaro@gmail.com>
2022-04-02 16:38:56 -0300

    JWT implemented using sqllite

--- InvitationPageAPI/Program.cs
+++ InvitationPageAPI/Program.cs
@@ -1,4 +1,37 @@
+using InvitationPageModel.DataModels;
+using InvitationPageModel.DataModels.Models.User;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static InvitationPageModel.DataModels.MyDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddSqlite<MyDbContext>(builder.Configuration.GetConnectionString("Default"))
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<MyDbContext>();
+
+builder.Services
+    .AddHttpContextAccessor()
+    .AddAuthorization()
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 // Add services to the container.
 
@@ -7,6 +40,89 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapPost("/token", async (AuthenticateRequest request, UserManager<User> userManager) =>
+{
+    // Verificamos credenciales con Identity
+    var user = await userManager.FindByNameAsync(request.UserName);
+
+    if (user is null || !await userManager.CheckPasswordAsync(user, request.Password))
+    {
+        return Results.Forbid();
+    }
+
+    var roles = await userManager.GetRolesAsync(user);
+
+    // Generamos un token según los claims
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Sid, user.Id),
+        new Claim(ClaimTypes.Name, user.UserName),
+        new Claim(ClaimTypes.GivenName, $"{user.FirstName} {user.LastName}")
+    };
+
+    foreach (var role in roles)
+    {
+        claims.Add(new Claim(ClaimTypes.Role, role));
+    }
+
+    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+    var tokenDescriptor = new JwtSecurityToken(
+        issuer: builder.Configuration["Jwt:Issuer"],
+        audience: builder.Configuration["Jwt:Audience"],
+        claims: claims,
+        expires: DateTime.Now.AddMinutes(720),
+        signingCredentials: credentials);
+
+    var jwt = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+
+    return Results.Ok(new
+    {
+        AccessToken = jwt
+    });
+});
+
+async Task SeedData()
+{
+    var scopeFactory = app!.Services.GetRequiredService<IServiceScopeFactory>();
+    using var scope = scopeFactory.CreateScope();
+
+    var context = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    context.Database.EnsureCreated();
+
+    if (!userManager.Users.Any())
+    {
+        logger.LogInformation("Creando usuario de prueba");
+
+        var newUser = new User
+        {
+            Email = "test@demo.com",
+            FirstName = "Test",
+            LastName = "User",
+            UserName = "test.demo"
+        };
+
+        await userManager.CreateAsync(newUser, "P@ss.W0rd");
+        await roleManager.CreateAsync(new IdentityRole
+        {
+            Name = "Admin"
+        });
+        await roleManager.CreateAsync(new IdentityRole
+        {
+            Name = "AnotherRole"
+        });
+
+        await userManager.AddToRoleAsync(newUser, "Admin");
+        await userManager.AddToRoleAsync(newUser, "AnotherRole");
+    }
+}
+
+await SeedData();
 
 // Configure the HTTP request pipeline.
 
@@ -21,3 +137,4 @@ app.MapSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+
---
+++ InvitationPageModel/DataModels/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvitationPageModel.DataModels.Models.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InvitationPageModel.DataModels
+{
+    public class MyDbContext : IdentityDbContext<User>
+    {
+        public record AuthenticateRequest(string UserName, string Password);
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
